06aea550aeae0568b757b8c6209e4e634cabc2db
alame001 <141000873+alame001@users.noreply.github.com>
2023-08-20 15:59:55 +0530

    WordCounter.java

--- WordCounter.java
+++ WordCounter.java
@@ -3,69 +3,62 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class WordCounter {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Word Count Tool!");
-        System.out.println("Enter '0' to input text, or '1' to read from a file:");
+        Scanner scanner = new Scanner(System.in);
 
-        try (BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(System.in))) {
-            String choice = reader.readLine();
+        System.out.println("Enter 'text' to input text, or 'file' to choose a file:");
+        String choice = scanner.nextLine().toLowerCase();
 
-            switch (choice) {
-                case "1":
-                    System.out.println("Enter your text:");
-                    String inputText = reader.readLine();
-                    processText(inputText);
-                    break;
-                case "2":
-                    System.out.println("Enter the path to the text file:");
-                    String filePath = reader.readLine();
-                    processFile(filePath);
-                    break;
-                default:
-                    System.out.println("Invalid choice.");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (choice.equals("text")) {
+            System.out.println("Enter your text:");
+            String inputText = scanner.nextLine();
+            processText(inputText);
+        } else if (choice.equals("file")) {
+            System.out.println("Choose a text file:");
+            String filePath = scanner.nextLine();
+            processFile(filePath);
+        } else {
+            System.out.println("Invalid choice.");
         }
+
+        scanner.close();
     }
 
     public static void processText(String text) {
-        String[] words = text.split("\\s+");
+        String[] words = text.split("\\s+|\\p{Punct}");
         int totalWords = words.length;
-        Map<String, Integer> wordFrequency = getWordFrequency(words);
+
+        Map<String, Integer> wordFrequency = new HashMap<>();
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
+            }
+        }
+
         int uniqueWords = wordFrequency.size();
 
         System.out.println("Total words: " + totalWords);
         System.out.println("Unique words: " + uniqueWords);
-        System.out.println("Word frequency:");
-
+        System.out.println("Word Frequency:");
         for (Map.Entry<String, Integer> entry : wordFrequency.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 
     public static void processFile(String filePath) {
-        try (BufferedReader fileReader = new BufferedReader(new FileReader(filePath))) {
-            StringBuilder contentBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            StringBuilder content = new StringBuilder();
             String line;
-            while ((line = fileReader.readLine()) != null) {
-                contentBuilder.append(line).append("\n");
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
             }
-            String fileContent = contentBuilder.toString();
-            processText(fileContent);
+            processText(content.toString());
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("An error occurred while reading the file: " + e.getMessage());
         }
-    }
-
-    public static Map<String, Integer> getWordFrequency(String[] words) {
-        Map<String, Integer> wordFrequency = new HashMap<>();
-        for (String word : words) {
-            word = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
-            wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
-        }
-        return wordFrequency;
     }
 }
